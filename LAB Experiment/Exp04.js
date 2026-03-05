7d84f9a72863cae1d02e8f1714e0129d93d59e72
anuradha-mudhale <anuradhamudhale314@gmail.com>
2026-03-04 23:57:31 -0800

    Exp4 added

--- LAB Experiment/Exp04.js
+++ LAB Experiment/Exp04.js
@@ -1,6 +1,9 @@
-function SubmitButtonClick(){
+function SubmitButtonClick(event){
+
+    event.preventDefault(); // prevent page reload
+
     console.log("SubmitButtonClick called");
-    //get user inputs using DOM
+
     let name = document.getElementById('name').value;
     let email = document.getElementById('email').value;
     let password = document.getElementById('password').value;
@@ -8,27 +11,37 @@ function SubmitButtonClick(){
     document.getElementById('nameError').innerHTML = "";
     document.getElementById('emailError').innerHTML = "";
     document.getElementById('passwordError').innerHTML = "";
+    document.getElementById('successMsg').innerHTML = "";
 
     let isValid = true;
 
-    if(name.trim() == ""){
+    if(name.trim() === ""){
         document.getElementById('nameError').innerHTML = "Name field is required";
         isValid = false;
     }
-    if(email.trim() == "" || !email.includes("@")){
-        document.getElementById('emailError').innerHTML = "Invalid Email";
-        isValid = false;
-    }
-    if(password.trim() == "" || password.length < 6){
-        document.getElementById('passwordError').innerHTML = "Invalid password";
+
+    if(email.trim() === "" || !email.includes("@")){
+        document.getElementById('emailError').innerHTML = "Enter valid email";
         isValid = false;
     }
 
-    if (isValid) {
+    if(password.trim() === "" || password.length < 6){
+        document.getElementById('passwordError').innerHTML = "Password must be at least 6 characters";
+        isValid = false;
+    }
+
+    if(isValid){
+        alert("form submitted successfully")
+        document.getElementById('successMsg').style.color = "green";
+
+        // DOM Manipulation Example
+        document.body.style.backgroundColor = "#e6ffe6";
+
+        // Clear fields
         document.getElementById('name').value = "";
         document.getElementById('email').value = "";
         document.getElementById('password').value = "";
     }
-   
-    
+
+    return false; // stop default submission
 }
