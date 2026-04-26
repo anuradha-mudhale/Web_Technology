d81edfe4420867e7954c8e68a49646892f31c442
anuradha-mudhale <anuradhamudhale314@gmail.com>
2026-04-26 11:47:31 +0530

    Exp9 Added

---
+++ LAB Experiment/Angular/Exp9/studentApp/src/list-student/list-student.ts
@@ -0,0 +1,40 @@
+import { Component } from '@angular/core';
+import { Student } from '../Services/student';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-list-student',
+  imports: [FormsModule],
+  templateUrl: './list-student.html',
+  styleUrl: './list-student.css',
+})
+export class ListStudent {
+ students: any[] = [];
+  editIndex: number = -1;
+
+  updatedStudent = {
+    name: '',
+    age: '',
+    course: ''
+  };
+
+  constructor(private StudentService : Student){}
+
+  ngOnInit(){
+    this.students = this.StudentService.getStudents();
+  }
+
+  deleteStudent(index: number){
+    this.StudentService.deleteStudent(index);
+  }
+
+  editStudent(index: number){
+    this.editIndex = index;
+    this.updatedStudent = {...this.students[index]};
+  }
+
+  updateStudent(){
+    this.StudentService.updateStudent(this.editIndex, this.updatedStudent);
+    this.editIndex = -1;
+  }
+}
